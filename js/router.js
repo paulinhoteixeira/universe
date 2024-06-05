19c77e39a4d79f5cbe8c4c70f1b02a7b07676569
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-06-04 23:13:38 -0300

    if to change bg-image

--- js/router.js
+++ js/router.js
@@ -9,15 +9,31 @@ export class Router{
     event = event || window.event
     event.preventDefault()
   
-    window.history.pushState({}, "", event.target.href)
+    window.history.pushState({}, "", event.target.href)   
+
+    if(event.target.href == "http://localhost:3000/universe"){
+      document.querySelector("html").classList.add("universe")
+      document.querySelector("html").classList.remove("explorer")
+    }
+
+    if(event.target.href == "http://localhost:3000/explorer"){
+      document.querySelector("html").classList.add("explorer")
+      document.querySelector("html").classList.remove("universe")
+    }
+
+    if(event.target.href == "http://localhost:3000/"){
+      document.querySelector("html").classList.remove("explorer")
+      document.querySelector("html").classList.remove("universe")
+    }
+    
   
-    this.handle()
-  
+    this.handle()  
   }
 
   handle(){
     const pathname = window.location.pathname
     const route = this.routes[pathname] || this.routes[404]
+    
   
     fetch(route)
       .then(data => data.text())
