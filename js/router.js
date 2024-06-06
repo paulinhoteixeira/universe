c4bd42b1f568e98552cbe990ee84050f2e6de384
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-06-05 22:21:40 -0300

    selected button style

--- js/router.js
+++ js/router.js
@@ -14,16 +14,25 @@ export class Router{
     if(event.target.href == "http://localhost:3000/universe"){
       document.querySelector("html").classList.add("universe")
       document.querySelector("html").classList.remove("explorer")
+      document.querySelector("a.universe").classList.add("selected")
+      document.querySelector("a.home").classList.remove("selected")
+      document.querySelector("a.explorer").classList.remove("selected")
     }
 
     if(event.target.href == "http://localhost:3000/explorer"){
       document.querySelector("html").classList.add("explorer")
       document.querySelector("html").classList.remove("universe")
+      document.querySelector("a.universe").classList.remove("selected")
+      document.querySelector("a.home").classList.remove("selected")
+      document.querySelector("a.explorer").classList.add("selected")
     }
 
     if(event.target.href == "http://localhost:3000/"){
       document.querySelector("html").classList.remove("explorer")
       document.querySelector("html").classList.remove("universe")
+      document.querySelector("a.universe").classList.remove("selected")
+      document.querySelector("a.home").classList.add("selected")
+      document.querySelector("a.explorer").classList.remove("selected")
     }
     
   
